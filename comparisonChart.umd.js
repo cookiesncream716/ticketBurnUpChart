43c2653ee19be6c191e005f537a58e47fce07a11
Heather Walker <cookiesncream@ladle.net>
2017-10-23 16:32:06 -0700

    stlye lines, readable date

--- comparisonChart.umd.js
+++ comparisonChart.umd.js
@@ -102,8 +102,6 @@ registerPlugin(proto(Gem, function(){
 		// completed date - (if archived true or if done true) date in history that done went from false to true
 		this.startDate
 		this.now = Math.round(new Date().getTime()/1000.0)
-		console.log('now = ', this.now)
-
 		if(ticket.subject.parent === undefined){
 			// this is top level ticket
 			this.startDate = ticket.subject.history[0].date
@@ -130,19 +128,11 @@ registerPlugin(proto(Gem, function(){
 		var xAxis = [this.startDate]
 		var y1 = [0]
 		var y2 = [0]
-		// var xAxis = [
-		// 	this.startDate,
-		// 	(this.startDate + (this.now-this.startDate)/4),
-		// 	(this.startDate + 2*((this.now-this.startDate)/4)),
-		// 	(this.startDate + 3*((this.now-this.startDate)/4)),
-		// 	this.now
-		// 	]
 		for(var x=1; x<weeks; x++){
 			xAxis.push(this.startDate + x*((this.now-this.startDate)/(weeks-1)))
 			y1.push(0)
 			y2.push(0)
 		}
-		// need to have as many if statements as weeks-1
 		this.children.forEach(function(child){
 			console.log('child ' , child)
 			var count = 0
@@ -161,20 +151,44 @@ registerPlugin(proto(Gem, function(){
 		console.log('total y1=' + y1)
 		console.log('open y2=' + y2)
 
+		var xAxisDate = []
+		for(var i=0; i<xAxis.length; i++){
+			xAxisDate.push((new Date(xAxis[i] * 1000).getMonth() + 1) + '/' + new Date(xAxis[i] * 1000).getDate() + '/' + new Date(xAxis[i] * 1000).getFullYear()) 
+		}
+
 		var line1 = {
-			x: xAxis,
+			x: xAxisDate,
 			y: y1,
 			type: 'scatter',
-			name: 'Total Tickets'
+			name: 'Total Tickets',
+			mode: 'lines+markers',
+			marker: {
+    			color: 'rgb(23, 156, 56)',
+    			size: 8
+			},
+    		line: {
+    			color: 'rgb(23, 156, 56)',
+    			width: 1
+  			}
 		}
 		var line2 = {
-			x: xAxis,
+			x: xAxisDate,
 			y: y2,
 			type: 'scatter',
-			name: 'Open Tickets'
+			name: 'Open Tickets',
+			mode: 'lines+markers',
+			marker: {
+				color: 'rgb(255,140,0)',
+				size: 8
+			},
+			line: {
+				color: 'rgb(255,140,0)',
+				width: 1
+			}
 		}
 		var lines = [line1, line2]
 		var layout = {
+			title: 'Ticket Comparison',
 			xaxis: {title: 'Date'},
 			yaxis: {title: 'Tickets'},
 			margin: {t:23}
@@ -196,7 +210,7 @@ registerPlugin(proto(Gem, function(){
 						}
 					}
 				} else{
-					data['completed'] = that.now + 10
+					data['completed'] = that.now + 100
 				}
 				that.children.push(data)
 			})
@@ -221,7 +235,57 @@ registerPlugin(proto(Gem, function(){
 // https://www.epochconverter.com/
 // var myDate = new Date( your epoch date *1000);
 
-
+// create graph
+		// var xAxis = [
+		// 	this.startDate,
+		// 	(this.startDate + (this.now-this.startDate)/4),
+		// 	(this.startDate + 2*((this.now-this.startDate)/4)),
+		// 	(this.startDate + 3*((this.now-this.startDate)/4)),
+		// 	this.now
+		// 	]
+		// var y1 = [0, 0, 0, 0, 0]
+		// var y2 = [0, 0, 0, 0, 0]
+		// console.log('children ' , this.children)
+		// this.children.forEach(function(child){
+		// 	if(child['created'] <= xAxis[0]){
+		// 		y1[0]++
+		// 		if(child['completed'] >= xAxis[0]){
+		// 			y2[0]++
+		// 		}
+		// 	}
+		// 	if(child['created'] <= xAxis[1]){
+		// 		y1[1]++
+		// 		if(child['completed'] >= xAxis[1]){
+		// 			y2[1]++
+		// 		}
+		// 	}
+		// 	if(child['created'] <= xAxis[2]){
+		// 		y1[2]++
+		// 		if(child['completed'] >= xAxis[2]){
+		// 			y2[2]++
+		// 		}
+		// 	}
+		// 	if(child['created'] <= xAxis[3]){
+		// 		y1[3]++
+		// 		if(child['completed'] >= xAxis[3]){
+		// 			y2[3]++
+		// 		}
+		// 	}
+		// 	if(child['created'] <= xAxis[4]){
+		// 		y1[4]++
+		// 		if(child['completed'] >= xAxis[4]){
+		// 			y2[4]++
+		// 		}
+		// 	}
+		// })
+		// var start = this.startDate * 1000
+		// xAxis = [
+		// 	(new Date(start).getMonth()+1) + '/' + new Date(start).getDate() + '/' + new Date(start).getFullYear(),
+		// 	(new Date(start+(this.now-this.startDate)/4).getMonth()+1) + '/' + new Date(start+(this.now-this.startDate)/4).getDate() + '/' + new Date(start+(this.now-this.startDate)/4).getFullYear(),
+		// 	(new Date(start+2*((this.now-this.startDate)/4)).getMonth()+1) + '/' + new Date(start+2*((this.now-this.startDate)/4)).getDate() + '/' + new Date(start+2*((this.now-this.startDate)/4)).getFullYear(),
+		// 	(new Date(start+3*((this.now-this.startDate)/4)).getMonth()+1) + '/' + new Date(start+3*((this.now-this.startDate)/4)).getDate() + '/' + new Date(start+3*((this.now-this.startDate)/4)).getFullYear(),
+		// 	(new Date(this.now*1000).getMonth()+1) + '/' + new Date(this.now*1000).getDate() + '/' + new Date(this.now*1000).getFullYear()
+		// 	]
 
 /***/ }),
 /* 1 */
