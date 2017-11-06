7c976c10479383584d80e3c907b5ccd2f4263906
Heather Walker <cookiesncream@ladle.net>
2017-11-06 15:46:30 -0800

    break case

--- comparisonChart.js
+++ comparisonChart.js
@@ -17,7 +17,7 @@ registerPlugin(proto(Gem, function(){
 		// completed date - (if archived true or if done true) date in history that done went from false to true
 		this.now = Math.round(new Date().getTime()/1000.0)
 
-		// Ticket and it's immediate children
+		// Displays Ticket's immediate children
 		this.startDate = ticket.subject.history[0].date
 		this.title = ticket.subject.title
 		this.createData(ticket.subject._id).then(function(){
@@ -53,6 +53,7 @@ registerPlugin(proto(Gem, function(){
 				var data = {}
 				data['created'] = child.subject.history[0].date
 				if(child.subject.done === true || child.subject.archived === true){
+					// not sure this is how history should work
 					if(child.subject.history.length === 1){
 						data['completed'] = child.subject.history[0].date
 					}else {
@@ -73,46 +74,60 @@ registerPlugin(proto(Gem, function(){
 	}
 
 	this.createGraph = function(){
+		var that = this
 		console.log('createGraph ', this.children)
 		// epoch time day = 86,400 seconds
 		// epoch time week = 604,800 seconds
 		var timeDiff = this.now-this.startDate
 		var week = 604800
-		// don't think this works to see chart same day that ticket has been createdß
 		if(Math.round(timeDiff/week) < 3){			// DAILY PLOT
+			console.log('daily')
 			var plots = Math.round(timeDiff/86400)
 		} else if(Math.round(timeDiff/week) < 13 ){ // WEEKLY PLOT
+			console.log('weekly')
 			var plots = Math.round(timeDiff/week)
 		} else if(Math.round(timeDiff/week) < 26){	// BIWEEKLY PLOT
+			console.log('biweekly')
 			var plots = Math.round(timeDiff/(week*2))
 		} else{										// MONTHLY PLOT
+			console.log('monthly')
 			var plots = Math.round(timeDiff/(week*4))
 		}
-		console.log('plots = ', plots)
-		// Create x and y axis for Markers
 		// xAxis.length = plots
-		var xAxis = [this.startDate]
-		var y1 = [0]
-		var y2 = [0]
-		// put dates in xAxis and make y axis the right length
+		console.log('plots = ', plots)
+
+		// Create x and y axis for Markers
+		var xAxis = [this.startDate] // dates
+		var y1 = [0]  // total tickets
+		var y2 = [0]  // open tickets
+		// Put dates in xAxis and make y axis the right length
 		for(var x=1; x<plots; x++){
 			xAxis.push(this.startDate + x*((this.now-this.startDate)/(plots-1)))
 			y1.push(0)
 			y2.push(0)
 		}
-		// find number of tickets and wether they are open or closed
-		this.children.forEach(function(child){
-			var count = 0
-			while(count <= plots){
-				if(child['created'] <= xAxis[count]){
-					y1[count]++
-					if(child['completed'] >= xAxis[count]){
-						y2[count]++
-					}
+
+		// Find number of tickets and wether they are open or closed
+		// problem - xAxis[0] is date ticket created but any child tickets created on same day will still have a later created date and if child ticket completed on same day
+		if(xAxis.length === 1){		//break case
+			y1[0] = this.children.length
+			this.children.forEach(function(child){
+				if(child['completed'] >= xAxis[0]){
+					y2[0]++
 				}
-				count++
-			}
-		})
+			})
+		} else{
+			this.children.forEach(function(child){
+				xAxis.forEach(function(x, index){
+					if(child['created'] <= x){
+						y1[index]++
+						if(child['completed'] >= x){
+							y2[index]++
+						}
+					}
+				})
+			})
+		}
 
 		// Make xAxis dates readable
 		var xAxisDate = []
@@ -163,14 +178,4 @@ registerPlugin(proto(Gem, function(){
 		}
 		plotly.newPlot(this.chart, lines, layout, options)
 	}
-
-	// this.getStyle = function(){
-	// 	return Style({
-	// 		Block: {
-	// 			width: '100%',
-	// 			minHeight: 250,
-	// 			paddingTop: 10
-	// 		}
-	// 	})
-	// }
 }))
