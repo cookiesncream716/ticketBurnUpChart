ef031c1de8624969677b3177d49fa77f11211794
Heather Walker <cookiesncream@ladle.net>
2017-11-08 16:07:17 -0800

    make createGraph more understandable

--- comparisonChart.umd.js
+++ comparisonChart.umd.js
@@ -105,17 +105,17 @@ registerPlugin(proto(Gem, function(){
 		this.title = ticket.subject.title
 
 		// Displays Ticket's immediate children
-		// this.createData(ticket.subject._id).then(function(){
+		// this.createDataChildren(ticket.subject._id).then(function(){
 		// 	return that.createGraph()
 		// }).done()
 
 		// Display all of Ticket's descendents
-		this.createDataAll(ticket.subject._id).then(function(){
+		this.createData(ticket.subject._id).then(function(){
 			return that.createGraph()
 		}).done()
 	}
 
-	// this.createData = function(id){
+	// this.createDataChildren = function(id){
 	// 	var that = this
 	// 	return this.api.Ticket.search({parent: id}).then(function(childTickets){
 	// 		console.log('child tickets ', childTickets)
@@ -143,7 +143,7 @@ registerPlugin(proto(Gem, function(){
 	// 	})
 	// }
 
-	this.createDataAll = function(id){
+	this.createData = function(id){
 		var that = this
 		return this.api.Ticket.search({$or: [{parent: id},{ancestry: id}]}).then(function(allTickets){
 			console.log('all descendents ', allTickets)
@@ -176,30 +176,28 @@ registerPlugin(proto(Gem, function(){
 		// epoch time day = 86,400 seconds
 		// epoch time week = 604,800 seconds
 		var timeDiff = this.now-this.startDate
+		var day = 86400
 		var week = 604800
-		if(Math.round(timeDiff/week) < 3){			// DAILY PLOT
-			console.log('daily')
-			var plots = Math.round(timeDiff/86400)
-		} else if(Math.round(timeDiff/week) < 13 ){ // WEEKLY PLOT
-			console.log('weekly')
-			var plots = Math.round(timeDiff/week)
-		} else if(Math.round(timeDiff/week) < 26){	// BIWEEKLY PLOT
-			console.log('biweekly')
-			var plots = Math.round(timeDiff/(week*2))
-		} else{										// MONTHLY PLOT
-			console.log('monthly')
-			var plots = Math.round(timeDiff/(week*4))
+		var numOfWeeks = Math.round(timeDiff/week)
+		if(numOfWeeks < 3){	// DAILY PLOT
+			var timePeriod = day
+		} else if(numOfWeeks < 13 ){ // WEEKLY PLOT
+			var timePeriod = week
+		} else if(numOfWeeks < 26){	// BIWEEKLY PLOT
+			var timePeriod = week * 2
+		} else{	// MONTHLY PLOT
+			var timePeriod = week * 4
 		}
-		// xAxis.length = plots
-		console.log('plots = ', plots)
+		var numOfMarkers = Math.round(timeDiff/timePeriod) 
+		// xAxis.length = numOfMarkers
 
 		// Create x and y axis for Markers
 		var xAxis = [this.startDate] // dates
 		var y1 = [0]  // total tickets
 		var y2 = [0]  // open tickets
 		// Put dates in xAxis and make y axis the right length
-		for(var x=1; x<plots; x++){
-			xAxis.push(this.startDate + x*((this.now-this.startDate)/(plots-1)))
+		for(var x=1; x<numOfMarkers; x++){
+			xAxis.push(this.startDate + x*((this.now-this.startDate)/(numOfMarkers-1)))
 			y1.push(0)
 			y2.push(0)
 		}
@@ -234,6 +232,7 @@ registerPlugin(proto(Gem, function(){
 		for(var i=0; i<xAxis.length; i++){
 			xAxisDate.push((new Date(xAxis[i] * 1000).getMonth() + 1) + '/' + new Date(xAxis[i] * 1000).getDate() + '/' + new Date(xAxis[i] * 1000).getFullYear()) 
 		}
+
 		var line1 = {
 			x: xAxisDate,
 			y: y1,
@@ -269,10 +268,8 @@ registerPlugin(proto(Gem, function(){
 			title: this.title,
 			xaxis: {title: 'Date'},
 			yaxis: {title: 'Tickets'}
-			// margin: {t: 43}
 		}
 		var options = {
-			// scrollZoom: true,
 			displaylogo: false,
 			modeBarButtonsToRemove: ['sendDataToCloud', 'zoom2d','hoverClosestCartesian', 'lasso2d']
 		}
